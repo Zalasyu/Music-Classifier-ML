3720ddc79308020111f3bbb7ca01b986ff789ca1
Zalasyu <alec_moldovan@yahoo.com>
2022-09-24 11:34:48 -0500

    Created test file usig pytest.

---
+++ tests/test_app.py
@@ -0,0 +1,24 @@
+"""
+Description:
+
+"""
+
+# Futures
+
+# Built-in/Generic Imports
+
+# Third-party Libs
+import pytest
+
+# Own Modules
+
+__author__ = 'Alec Moldovan'
+__copyright__ = 'Copyright 2022, project_name'
+__credits__ = ['credit_list']
+__license__ = 'license'
+__version__ = 'mayor.minor.rel'
+__maintainer__ = 'Alec Moldovan'
+__email__ = 'alec_moldovan@yahoo.com'
+__status__ = 'dev_status'
+
+
