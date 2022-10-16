a41b28638e2307cab593520a1f27ff90bac2d794
Zalasyu <alec_moldovan@yahoo.com>
2022-10-16 14:24:15 -0500

    Commented out mpy and pylint from .pre-commit file.

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,14 +11,14 @@ Description:
 
 # Own Modules
 
-__author__ = 'Alec Moldovan'
-__copyright__ = 'Copyright 2022, project_name'
-__credits__ = ['credit_list']
-__license__ = 'license'
-__version__ = 'mayor.minor.rel'
-__maintainer__ = 'Alec Moldovan'
-__email__ = 'alec_moldovan@yahoo.com'
-__status__ = 'dev_status'
+__author__ = "Alec Moldovan"
+__copyright__ = "Copyright 2022, Music-Classifier-ML"
+__credits__ = ["Alec Moldovan"]
+__license__ = "license"
+__version__ = "1.0.0"
+__maintainer__ = "Alec Moldovan"
+__email__ = "alec_moldovan@yahoo.com"
+__status__ = "dev_status"
 
 
 def test_main():
