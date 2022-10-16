09c9705e1d3071c4d3b33e993ca3dce3bddcf7ae
Zalasyu <alec_moldovan@yahoo.com>
2022-10-16 17:59:12 -0500

    Black lint checking failing?

---
+++ src/data/make_dataset.py
@@ -0,0 +1,9 @@
+from torch.utils.data import Dataset
+
+
+class GitzanDataset(Dataset):
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,3 +3,9 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transform import ToTensor
+
+# 1 - Download dataset
+# 2 - Create data loader
+# 3 - Build model
+# 4 - Train
+# 5 - Save Trained model
