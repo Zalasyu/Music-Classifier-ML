3a2b3d02412082fa79b4941d47ff26b72fe68da6
Alli <allisonglittle@gmail.com>
2022-10-17 19:27:09 -0700

    Initial genre dictionary private method

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,17 +1,19 @@
 import os
-# import cv2 as cv
+import cv2 as cv
 import numpy as np
 
 REBUILD_DATA = True
 
 
-class MusicData():
-    RAWDATA = "/raw-data"
+class MusicData:
+    RAWDATA = "raw-data"
     training_data = []
+    genre_dict = {}
 
     def make_training_data(self):
-        for f in os.listdir(self.RAWDATA):
-            path = os.path.join(self.RAWDATA, f)
+        self.__create_genre_dictionary()
+        # for f in os.listdir(self.RAWDATA):
+            # path = os.path.join(self.RAWDATA, f)
             # img = cv.imread(path, cv.IMREAD_GRAYSCALE)
             # call Librosa function
 
@@ -20,6 +22,13 @@ class MusicData():
 
         np.random.shuffle(self.training_data)
         np.save("training_data.npy", self.training_data)
+
+    def __create_genre_dictionary(self):
+        self.genre_dict = {}
+        genre_count = 0
+        for g in os.listdir(self.RAWDATA):
+            self.genre_dict[g] = genre_count
+            genre_count += 1
 
 
 if REBUILD_DATA:
