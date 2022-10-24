49e73b4a92ce8341825d8eca6e3a5d95f6d9f51c
Zalasyu <alec_moldovan@yahoo.com>
2022-10-24 15:41:45 -0500

    Removed unused imports from train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,9 +1,3 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transform import ToTensor
-
 # 1 - Download dataset
 # 2 - Create data loader
 # 3 - Build model
