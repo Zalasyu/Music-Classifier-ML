6fa641287d709ac3a9dc0524dff552589772e8cb
Zalasyu <alec_moldovan@yahoo.com>
2022-10-26 20:01:57 -0500

    Fixed test case

--- tests/tests_MGR.py
+++ tests/tests_MGR.py
@@ -9,6 +9,6 @@ class TestPrepareAudio():
     """
 
     def test_mp3_file_accepted(self):
-        """Test that a valid mp3 file is accepted
-        """
-        pass
+        """Test that a valid mp3 file is accepted"""
+        file = Path("tests/test_data/validfile_1.mp3")
+        assert pa.check_file_type(file) is True
